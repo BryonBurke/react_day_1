892b37630b7092d6d857fab45ced40aebf875e88
Student <example@example.com>
2020-02-10 15:54:12 -0800

    postition profile placeholder

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -14,30 +14,54 @@ function Profile(props){
     // backgroundImage:"linear-gradient(lightskyblue 60%, white 50%)"
     background:"lightskyblue",
     height:"100px",
-    width:"300px"
+    width:"330px",
+
+
   }
 
   var profileLinksStyle = {
     margin:"5px",
     fontSize:"10pt"
 
+
   }
 
   var bottomRowStyle = {
     border: "5px solid lightgrey",
     height:"100px",
-    width:"290px"
+    width:"320px",
+    color:"grey",
+    // position:"absolute",
+    // zIndex:"-1"
 
   }
+
+  var profilePicStyle = {
+    maxWidth: "75px",
+    maxHeight:"75px",
+    position:"absolute",
+    zIndex:"1",
+    top:"150px",
+    left:"20px",
+    borderRadius:"20px",
+    border:"solid white"
+  }
+
+  var nameStyle = {
+    position:"relative",
+    left:"100px"
+  }
+
   return (
     <div>
 
       <div style={backgroundTopStyle}>
 
       </div>
+      <img style={profilePicStyle} src="https://www.cloudraxak.com/wp-content/uploads/2017/03/profile-pic-placeholder.png"></img>
       <div style={bottomRowStyle}>
 
-        <h2>Kellie A. Corrigan</h2>
+        <h2 style={nameStyle}>Kellie A. Corrigan</h2>
         <span style={profileLinksStyle}>TWEETS</span>
         <span style={profileLinksStyle}>FOLLOWING</span>
         <span style={profileLinksStyle}>FOLLOWERS</span>
