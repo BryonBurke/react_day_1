513ab662a6b34c4f032de1094ef62694549680ac
Student <example@example.com>
2020-02-10 11:54:31 -0800

    complete nav bar

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+import TicketList from "./TicketList";
+
+function App(){
+  return (
+    <div>
+      <Navbar/>
+      <TicketList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Navbar(props){
+  var navbarStyles = {
+    border: "solid lightgrey",
+    borderWidth: "1px",
+    padding: "10px 20px",
+    textAlign: "center",
+    color: "grey",
+    margin: "60px 0px"
+
+  }
+  var searchStyle = {
+    border: "solid lightskyblue",
+    padding: "5px 150px 5px 10px",
+    borderRadius: "30px",
+    float: "right",
+    margin: "0px 50px 0px 0px",
+    color: "grey"
+  }
+
+  var tweetStyle = {
+    border: "solid lightskyblue",
+    padding: "5px 20px ",
+    borderRadius: "30px",
+    float: "right",
+    color: "grey"
+  }
+
+  var hrStyle = {
+    border: "1px solid lightgrey",
+    margin: "25px 0"
+
+
+  }
+
+
+  return (
+    <div>
+    <span style={navbarStyles}>Home</span>
+    <span style={navbarStyles}>Notifications</span>
+    <span style={navbarStyles}>Messages</span>
+    <span style={tweetStyle}>Tweet</span>
+    <span style={searchStyle}>Search</span>
+    <hr style={hrStyle}/>
+
+    </div>
+  );
+}
+
+export default Navbar;
